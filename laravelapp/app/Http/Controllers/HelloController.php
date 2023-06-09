53d7184a1efa8b56bcbcadf3e430f3a02a43b99b
Kisara Nishikawa <nishikawa@overwrite.work>
2023-06-09 09:41:32 +0900

    最初のプッシュ

---
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index() {
+        return <<<EOF
+        <html>
+        <head>
+        <title>Hello</title>
+        <style>
+        body {font-size:16pt; color:#999;}
+        h1 {font-size:30pt; text-align:right; color:#eee;
+            margin:-15px 0px 0px 0px}
+        </style>
+        </head>
+        <body>
+            <h1>Single Action</h1>
+            <p>これは、シングルアクションのコントローラです。</p>
+        </body>
+        </html>
+        EOF;
+    }
+}
